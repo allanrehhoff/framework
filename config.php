fca05fa818e8bac857de6d8a1d4c1c5e79cabfad
Allan Rehhoff <allan@sharksmedia.dk>
2015-03-30 18:34:06 +0200

    deleting this file to remove password from commit history.. will be added again later on.

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//Core settings
-	$config = array();
-	$config['theme'] = 'default';
-	$config['base_title'] = '%s - '; //Only one string parameter supported.
-	$config['default_route'] = 'index';
-?>
