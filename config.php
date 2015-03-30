b3f6dd08ed3c8a0554d7c5168f26595dbc518928
Allan Rehhoff <allan@sharksmedia.dk>
2015-03-30 18:26:08 +0200

    Removed password

--- config.php
+++ config.php
@@ -4,7 +4,4 @@
 	$config['theme'] = 'default';
 	$config['base_title'] = '%s - '; //Only one string parameter supported.
 	$config['default_route'] = 'index';
-
-	//Additional settings
-	$config['password'] = "***REMOVED***";
 ?>
