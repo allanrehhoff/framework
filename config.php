af3f3abcb20e0762f3b987249f239a0100a58818
Allan Rehhoff <allan@sharksmedia.dk>
2015-03-30 18:36:00 +0200

    Fixing what i screwed up before

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	//Core settings
+	$config = array();
+	$config['theme'] = 'default';
+	$config['base_title'] = '%s - '; //Only one string parameter supported.
+	$config['default_route'] = 'index';
+?>
