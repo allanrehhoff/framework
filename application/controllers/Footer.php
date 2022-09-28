9503f96c0432e09a4688b793c773be0de2e6cfde
allanrehhoff <allan.rehhoff@gmail.com>
2022-09-28 22:27:07 +0200

    Adding theme assets vars to child controllers

--- application/controllers/Footer.php
+++ application/controllers/Footer.php
@@ -1,10 +1,10 @@
 <?php
 	class FooterController extends Controller {
 		public function index() {
-			// If a footer has been set, e.g. by login page
-			// do not overwrite it.
-			if(!isset($this->data["footer"])) {
-				$this->data["footer"] = $this->getView("footer");
-			}
+			// Do not allow this controller to be access directly
+			if($this->getParent() == null) throw new \Core\NotFoundException();
+
+			$this->data["footer"] = $this->getView("footer");
+			$this->data["stylesheets"] = $this->iAssets->getStylesheets("header");
 		}
 	}
--- application/controllers/Header.php
+++ application/controllers/Header.php
@@ -4,10 +4,7 @@
 			// Do not allow this controller to be access directly
 			if($this->getParent() == null) throw new \Core\NotFoundException();
 
-			// If a header has been set, e.g. by a login page
-			// do not overwrite it.
-			if(!isset($this->data["header"])) {
-				$this->data["header"] = $this->getView("header");
-			}
+			$this->data["header"] = $this->getView("header");
+			$this->data["javascript"]  = $this->iAssets->getJavascript("footer");
 		}
 	}
