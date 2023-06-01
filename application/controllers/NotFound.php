df670ed758a7b5f64a93fbf24c0eac4551aaf558
allanrehhoff <allan.rehhoff@gmail.com>
2023-06-01 22:05:00 +0200

    Remove exit

--- application/controllers/NotFound.php
+++ application/controllers/NotFound.php
@@ -8,8 +8,6 @@
 		 * Constructs an indicates the path wasn't found
 		 */
 		public function index() {
-			if(IS_CLI) exit(1);
-
 			$this->response->data["httpHost"] = \HtmlEscape::escape($this->request->server["HTTP_HOST"] ?? '');
 			$this->response->data["requestUri"] = \HtmlEscape::escape($this->request->server["REQUEST_URI"] ?? '');
 			$this->response->data["httpReferer"] = \HtmlEscape::escape($this->request->server["HTTP_REFERER"] ?? '');
