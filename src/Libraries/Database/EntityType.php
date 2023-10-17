9f7f1820e4baab48ffbc265e308eb0460c47b7c8
allanrehhoff <allan.rehhoff@gmail.com>
2023-10-17 23:00:05 +0200

    Update database class

--- src/Libraries/Database/EntityType.php
+++ src/Libraries/Database/EntityType.php
@@ -1,15 +1,15 @@
 <?php
-	/**
-	* This file is solely used for demonstation purposes.
-	* Whenever you create a new CRUD'able instance that extends \Database\Entity it must contain at least the two methods defined in this class.
-	* However it does not neccesarily need to be in a namespace, as long as it extends \Database\Entity.
-	*
-	* getKeyField(); Must return the name of the column with the primary key index.
-	* getTableName(); Must return the name of the table this Entity operates with.
-	*/
-	namespace Database {	
-		class EntityType extends \Database\Entity {
-			protected static function getKeyField() : string { return "test_id"; }
-			protected static function getTableName() : string { return "test_table"; }
-		}
+/**
+* This file is solely used for demonstation purposes.
+* Whenever you create a new CRUD'able instance that extends \Database\Entity it must contain at least the two methods defined in this class.
+* However it does not neccesarily need to be in a namespace, as long as it extends \Database\Entity.
+*
+* getKeyField(); Must return the name of the column with the primary key index.
+* getTableName(); Must return the name of the table this Entity operates with.
+*/
+namespace Database {	
+	class EntityType extends \Database\Entity {
+		protected static function getKeyField() : string { return "test_id"; }
+		protected static function getTableName() : string { return "test_table"; }
 	}
+}
--- src/Libraries/Database/Statement.php
+++ src/Libraries/Database/Statement.php
@@ -1,47 +1,50 @@
 <?php
-	namespace Database {
-		class Statement extends \PDOStatement {
-			/**
-			 * Dear future me, PDOStatement has no __construct() method
-			 * No need not to add a parent::__construct(); call in here
-			 */
+namespace Database {
+	class Statement extends \PDOStatement {
+		/**
+		 * Dear future me, PDOStatement has no __construct() method
+		 * No need not to add a parent::__construct(); call in here
+		 * But this is still required in order to avoid this error:
+		 * "User-supplied statement does not accept constructor arguments"
+		 */
+		protected function __construct() {}
 
-			/**
-			 * Fetch the column queried
-			 * 
-			 * @return array
-			 */
-			public function fetchCol() : array {
-				$result = $this->fetchAll(\PDO::FETCH_COLUMN);
-				
-				// PHP < 8.0.0 compat. PDOStatement::fetchAll(); will return false
-				// if the result set was empty, fixed in PHP 8.0.0
-				return $result !== false ? $result : [];
-			}
+		/**
+		 * Fetch the column queried
+		 * 
+		 * @return array
+		 */
+		public function fetchCol() : array {
+			$result = $this->fetchAll(\PDO::FETCH_COLUMN);
+			
+			// PHP < 8.0.0 compat. PDOStatement::fetchAll(); will return false
+			// if the result set was empty, fixed in PHP 8.0.0
+			return $result !== false ? $result : []; // @phpstan-ignore-line
+		}
 
-			/**
-			 * Fetches the next row from a result set 
-			 * \PDOStatement::fetchColumn(); will return false
-			 * This method will make sure NULL may be return instead
-			 * @return mixed
-			 */
-			public function fetch(int $mode = \PDO::FETCH_DEFAULT, int $cursorOrientation = \PDO::FETCH_ORI_NEXT, int $cursorOffset = 0) : mixed {
-				$result = parent::fetch($mode, $cursorOrientation, $cursorOffset);
-				
-				// PDOStatement::fetchColumn(); will return false
-				// We'll normalise it to return null
-				return $result !== false ? $result : null;
-			}
+		/**
+		 * Fetches the next row from a result set 
+		 * \PDOStatement::fetchColumn(); will return false
+		 * This method will make sure NULL may be return instead
+		 * @return mixed
+		 */
+		public function fetch(int $mode = \PDO::FETCH_DEFAULT, int $cursorOrientation = \PDO::FETCH_ORI_NEXT, int $cursorOffset = 0) : mixed {
+			$result = parent::fetch($mode, $cursorOrientation, $cursorOffset);
+			
+			// PDOStatement::fetchColumn(); will return false
+			// We'll normalise it to return null
+			return $result !== false ? $result : null;
+		}
 
-			/**
-			 * Fetch a column by numeric index from the resultset
-			 * \PDOStatement::fetchColumn(); will return false
-			 * This method will make sure NULL may be return instead
-			 * @return mixed
-			 */
-			public function fetchColumn(int $column = 0): mixed {
-				$result = parent::fetchColumn($column);
-				return $result !== false ? $result : null;
-			}
+		/**
+		 * Fetch a column by numeric index from the resultset
+		 * \PDOStatement::fetchColumn(); will return false
+		 * This method will make sure NULL may be return instead
+		 * @return mixed
+		 */
+		public function fetchColumn(int $column = 0): mixed {
+			$result = parent::fetchColumn($column);
+			return $result !== false ? $result : null;
 		}
 	}
+}
