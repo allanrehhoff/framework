b4d77e03e59875b85df3ffb48a861d87afd29b36
allanrehhoff <allan.rehhoff@gmail.com>
2023-11-09 22:23:59 +0100

    Adding tests for singleton class

---
+++ tests/SingletonTest.php
@@ -0,0 +1,26 @@
+<?php
+	class SingletonTest extends PHPUnit\Framework\TestCase {
+		public function testSimpleObjectStorage() {
+			$iResponse = \ResponseFactory::new();
+
+			\Singleton::set($iResponse);
+
+			$this->assertInstanceOf(\Core\Response::class, \Singleton::get("Core\Response"));
+		}
+
+		public function testObjectAlias() {
+			$iResponse = \ResponseFactory::new();
+
+			\Singleton::set($iResponse, "response");
+
+			$this->assertInstanceOf(\Core\Response::class, \Singleton::get("response"));
+		}
+
+		public function testAliasAsFunctionCall() {
+			$iResponse = \ResponseFactory::new();
+
+			\Singleton::set($iResponse, "response");
+
+			$this->assertInstanceOf(\Core\Response::class, \Singleton::response());
+		}
+	}
